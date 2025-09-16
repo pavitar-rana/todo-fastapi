13d0534dabb5adada343abfe09b4c1ffd08cfc51
Pavitar Rana <ranapavitar14@gmail.com>
2025-09-16 17:27:51 +0530

    feat(api): add phone number field using alembic

---
+++ alembic/versions/41aaee03bfd4_create_phone_number_for_user_column.py
@@ -0,0 +1,30 @@
+"""Create phone number for user column
+
+Revision ID: 41aaee03bfd4
+Revises:
+Create Date: 2025-09-16 17:23:10.098099
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "41aaee03bfd4"
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.add_column("users", sa.Column("phone_number", sa.String(), nullable=True))
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
