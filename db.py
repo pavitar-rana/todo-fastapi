0175a65954b9e90754cb611900aeb8d3e76c82f7
Pavitar Rana <ranapavitar14@gmail.com>
2025-09-16 15:22:00 +0530

    feat(api): switched to postgresql from sqlite

--- db.py
+++ db.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///todosapp.db"
+# DATABASE_URL = "sqlite:///todosapp.db"
+DATABASE_URL = "postgresql://postgres:PavitarKhushi@db.bllnaolmucjcmqdfmrvj.supabase.co:5432/postgres"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, bind=engine)
 Base = declarative_base()
